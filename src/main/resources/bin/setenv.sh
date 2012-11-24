d559b170ecb908abdf79952a84335ca5ed20f0ad
Frederic DROUET <ftarpoon@gmail.com>
2012-11-24 01:03:41 +0100

    SWF-2162 : first version of the server.xml with variables

--- src/main/resources/bin/setenv.sh
+++ src/main/resources/bin/setenv.sh
@@ -19,23 +19,152 @@
 # 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 #
 
-# Only set CATALINA_HOME if not already set
-[ -z "$CATALINA_HOME" ] && CATALINA_HOME=`cd "$PRGDIR/.." >/dev/null; pwd`
+########################################
+# Settings customisation
+########################################
+# You have 2 ways to customize your installation settings :
+# 1- uncomment and change value to override settings in the above section
+# 2- use environment properties of the system to override the value
+########################################
+#EXO_JVM_VENDOR="IBM"
+#EXO_JVM_SIZE_MAX=2g
+#EXO_JVM_SIZE_MIN=1g
+#EXO_PROFILES="default"
+#EXO_HTTP_COMPRESSION="off"
 
-# Sets some variables
-LOG_OPTS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
-SECURITY_OPTS="-Djava.security.auth.login.config=${CATALINA_HOME}/conf/jaas.conf"
-EXO_OPTS="-Dexo.conf.dir.name=gatein/conf -Dexo.conf.dir=${CATALINA_HOME}/gatein/conf"
-IDE_OPTS="-Djavasrc=$JAVA_HOME/src.zip -Djre.lib=$JAVA_HOME/jre/lib"
-if [ "$EXO_PROFILES" = "" -o "$EXO_PROFILES" = "-Dexo.profiles=default" ] ; then
-	EXO_PROFILES="-Dexo.profiles=default"
+
+#=============================================================================#
+#           /!\     DON'T MODIFY BESIDE THIS LINE      /!\                    #
+#=============================================================================#
+
+########################################
+# Default EXO PLATFORM configuration
+########################################
+EXO_PROFILES=${EXO_PROFILES:-"default"}
+EXO_CONF_DIR_NAME=${EXO_CONF_DIR_NAME:-"gatein/conf"}
+EXO_CONF_DIR=${EXO_CONF_DIR:-"${CATALINA_HOME}/${EXO_CONF_DIR_NAME}"}
+
+########################################
+# Default JVM configuration
+########################################
+EXO_JVM_VENDOR=${EXO_JVM_VENDOR:-"ORACLE"}
+EXO_JVM_SIZE_MAX=${EXO_JVM_SIZE_MAX:-1g}
+EXO_JVM_SIZE_MIN=${EXO_JVM_SIZE_MIN:-512m}
+EXO_JVM_PERMSIZE_MAX=${EXO_JVM_PERMSIZE_MAX:-256m}
+EXO_JVM_PERMSIZE_MIN=${EXO_JVM_PERMSIZE_MIN:-128m}
+
+########################################
+# Default Various configuration
+########################################
+EXO_COMMONS_LOGGING_IMPL=${EXO_COMMONS_LOGGING_IMPL:-"org.apache.commons.logging.impl.SimpleLog"}
+
+########################################
+# Default Tomcat configuration
+########################################
+# Global Tomcat settings
+EXO_TOMCAT_SHUTDOWN_PORT=${EXO_TOMCAT_SHUTDOWN_PORT:-8005}
+EXO_TOMCAT_SHUTDOWN_KEY=${EXO_TOMCAT_SHUTDOWN_KEY:-"SHUTDOWN"}
+EXO_TOMCAT_REDIRECT_PORT=${EXO_TOMCAT_REDIRECT_PORT:-8443}
+EXO_TOMCAT_URI_ENCODING=${EXO_TOMCAT_URI_ENCODING:-"UTF-8"}
+EXO_TOMCAT_RMI_REGISTRY_PORT=${EXO_TOMCAT_RMI_REGISTRY_PORT:-10001}
+EXO_TOMCAT_RMI_SERVER_PORT=${EXO_TOMCAT_RMI_SERVER_PORT:-10002}
+EXO_TOMCAT_RMI_LOCAL_PORT=${EXO_TOMCAT_RMI_LOCAL_PORT:-"false"}
+EXO_TOMCAT_JVMROUTE_NAME=${EXO_TOMCAT_JVMROUTE_NAME:-""}
+
+# HTTP configuration
+EXO_HTTP_PORT=${EXO_HTTP_PORT:-8080}
+EXO_HTTP_ADDRESS=${EXO_HTTP_ADDRESS:-0.0.0.0}
+EXO_HTTP_PROTOCOL=${EXO_HTTP_PROTOCOL:-"org.apache.coyote.http11.Http11NioProtocol"}
+EXO_HTTP_CX_TIMEOUT=${EXO_HTTP_CX_TIMEOUT:-20000}
+EXO_HTTP_COMPRESSION=${EXO_HTTP_COMPRESSION:-"off"}
+EXO_HTTP_COMPRESSION_MIN_SIZE=${EXO_HTTP_COMPRESSION_MIN_SIZE:-2048}
+
+# AJP configuration
+EXO_AJP_PORT=${EXO_AJP_PORT:-8010}
+EXO_AJP_ADDRESS=${EXO_AJP_ADDRESS:-0.0.0.0}
+EXO_AJP_PROTOCOL=${EXO_AJP_PROTOCOL:-"AJP/1.3"}
+EXO_AJP_CX_TIMEOUT=${EXO_AJP_CX_TIMEOUT:-20000}
+
+########################################
+# Default Datasources configuration
+########################################
+# Datasource IDM
+EXO_DS_IDM_DRIVER=${EXO_DS_IDM_DRIVER:-"org.hsqldb.jdbcDriver"}
+EXO_DS_IDM_USERNAME=${EXO_DS_IDM_USERNAME:-"sa"}
+EXO_DS_IDM_PASSWORD=${EXO_DS_IDM_PASSWORD:-""}
+EXO_DS_IDM_MAX_ACTIVE=${EXO_DS_IDM_MAX_ACTIVE:-20}
+EXO_DS_IDM_MAX_IDLE=${EXO_DS_IDM_MAX_IDLE:-10}
+EXO_DS_IDM_MAX_WAIT=${EXO_DS_IDM_MAX_WAIT:-10000}
+EXO_DS_IDM_URL=${EXO_DS_IDM_URL:-"jdbc:hsqldb:file:${CATALINA_HOME}/gatein/data/hsql/exo-idm_portal"}
+
+# Datasource PORTAL
+EXO_DS_PORTAL_DRIVER=${EXO_DS_PORTAL_DRIVER:-"org.hsqldb.jdbcDriver"}
+EXO_DS_PORTAL_USERNAME=${EXO_DS_PORTAL_USERNAME:-"sa"}
+EXO_DS_PORTAL_PASSWORD=${EXO_DS_PORTAL_PASSWORD:-""}
+EXO_DS_PORTAL_MAX_ACTIVE=${EXO_DS_PORTAL_MAX_ACTIVE:-20}
+EXO_DS_PORTAL_MAX_IDLE=${EXO_DS_PORTAL_MAX_IDLE:-10}
+EXO_DS_PORTAL_MAX_WAIT=${EXO_DS_PORTAL_MAX_WAIT:-10000}
+EXO_DS_PORTAL_URL=${EXO_DS_PORTAL_URL:-"jdbc:hsqldb:file:${CATALINA_HOME}/gatein/data/hsql/exo-jcr_portal"}
+
+########################################
+# Export the needed system properties for server.xml
+########################################
+EXO_SERVER_XML_OPTS="-DEXO_TOMCAT_SHUTDOWN_PORT=${EXO_TOMCAT_SHUTDOWN_PORT}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_TOMCAT_SHUTDOWN_KEY=${EXO_TOMCAT_SHUTDOWN_KEY}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_TOMCAT_REDIRECT_PORT=${EXO_TOMCAT_REDIRECT_PORT}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_TOMCAT_URI_ENCODING=${EXO_TOMCAT_URI_ENCODING}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_TOMCAT_RMI_REGISTRY_PORT=${EXO_TOMCAT_RMI_REGISTRY_PORT}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_TOMCAT_RMI_SERVER_PORT=${EXO_TOMCAT_RMI_SERVER_PORT}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_TOMCAT_RMI_LOCAL_PORT=${EXO_TOMCAT_RMI_LOCAL_PORT}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_TOMCAT_JVMROUTE_NAME=${EXO_TOMCAT_JVMROUTE_NAME}"
+
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_HTTP_PORT=${EXO_HTTP_PORT}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_HTTP_ADDRESS=${EXO_HTTP_ADDRESS}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_HTTP_PROTOCOL=${EXO_HTTP_PROTOCOL}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_HTTP_CX_TIMEOUT=${EXO_HTTP_CX_TIMEOUT}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_HTTP_COMPRESSION=${EXO_HTTP_COMPRESSION}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_HTTP_COMPRESSION_MIN_SIZE=${EXO_HTTP_COMPRESSION_MIN_SIZE}"
+
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_AJP_PORT=${EXO_AJP_PORT}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_AJP_ADDRESS=${EXO_AJP_ADDRESS}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_AJP_PROTOCOL=${EXO_AJP_PROTOCOL}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_AJP_CX_TIMEOUT=${EXO_AJP_CX_TIMEOUT}"
+
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_DS_IDM_DRIVER=${EXO_DS_IDM_DRIVER}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_DS_IDM_USERNAME=${EXO_DS_IDM_USERNAME}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_DS_IDM_PASSWORD=${EXO_DS_IDM_PASSWORD}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_DS_IDM_MAX_ACTIVE=${EXO_DS_IDM_MAX_ACTIVE}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_DS_IDM_MAX_IDLE=${EXO_DS_IDM_MAX_IDLE}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_DS_IDM_MAX_WAIT=${EXO_DS_IDM_MAX_WAIT}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_DS_IDM_URL=${EXO_DS_IDM_URL}"
+
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_DS_PORTAL_DRIVER=${EXO_DS_PORTAL_DRIVER}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_DS_PORTAL_USERNAME=${EXO_DS_PORTAL_USERNAME}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_DS_PORTAL_PASSWORD=${EXO_DS_PORTAL_PASSWORD}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_DS_PORTAL_MAX_ACTIVE=${EXO_DS_PORTAL_MAX_ACTIVE}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_DS_PORTAL_MAX_IDLE=${EXO_DS_PORTAL_MAX_IDLE}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_DS_PORTAL_MAX_WAIT=${EXO_DS_PORTAL_MAX_WAIT}"
+EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -DEXO_DS_PORTAL_URL=${EXO_DS_PORTAL_URL}"
+
+#EXO_SERVER_XML_OPTS="${EXO_SERVER_XML_OPTS} -D=${}"
+
+########################################
+# Compute the CATALINA_OPTS
+########################################
+CATALINA_OPTS="-Dexo.profiles=${EXO_PROFILES}"
+CATALINA_OPTS="${CATALINA_OPTS} -Xms${EXO_JVM_SIZE_MIN} -Xmx${EXO_JVM_SIZE_MAX} -XX:MaxPermSize=${EXO_JVM_PERMSIZE_MAX}"
+CATALINA_OPTS="${CATALINA_OPTS} -Dorg.apache.commons.logging.Log=${EXO_COMMONS_LOGGING_IMPL}"
+CATALINA_OPTS="${CATALINA_OPTS} -Djava.security.auth.login.config=${CATALINA_HOME}/conf/jaas.conf"
+CATALINA_OPTS="${CATALINA_OPTS} -Dexo.conf.dir.name=${EXO_CONF_DIR_NAME} -Dexo.conf.dir=${EXO_CONF_DIR}"
+CATALINA_OPTS="${CATALINA_OPTS} -Djavasrc=${JAVA_HOME}/src.zip -Djre.lib=${JAVA_HOME}/jre/lib"
+# Define the XML Parser depending on the JVM vendor
+if [ "${EXO_JVM_VENDOR}" == "IBM" ]; then
+  CATALINA_OPTS="${CATALINA_OPTS} -Djavax.xml.stream.XMLOutputFactory=com.sun.xml.stream.ZephyrWriterFactory -Djavax.xml.stream.XMLInputFactory=com.sun.xml.stream.ZephyrParserFactory -Djavax.xml.stream.XMLEventFactory=com.sun.xml.stream.events.ZephyrEventFactory"
+else
+  CATALINA_OPTS="${CATALINA_OPTS} -Djavax.xml.stream.XMLOutputFactory=com.sun.xml.internal.stream.XMLOutputFactoryImpl -Djavax.xml.stream.XMLInputFactory=com.sun.xml.internal.stream.XMLInputFactoryImpl -Djavax.xml.stream.XMLEventFactory=com.sun.xml.internal.stream.events.XMLEventsFactoryImpl"
 fi
-
-##### For XML Parser ####
-# Define the preferred XML Parser
-# If you run eXo Platform on IBM Java, you must use the first one
-#EXO_XML="-Djavax.xml.stream.XMLOutputFactory=com.sun.xml.stream.ZephyrWriterFactory -Djavax.xml.stream.XMLInputFactory=com.sun.xml.stream.ZephyrParserFactory -Djavax.xml.stream.XMLEventFactory=com.sun.xml.stream.events.ZephyrEventFactory"
-EXO_XML="-Djavax.xml.stream.XMLOutputFactory=com.sun.xml.internal.stream.XMLOutputFactoryImpl -Djavax.xml.stream.XMLInputFactory=com.sun.xml.internal.stream.XMLInputFactoryImpl -Djavax.xml.stream.XMLEventFactory=com.sun.xml.internal.stream.events.XMLEventsFactoryImpl"
-#########################
-CATALINA_OPTS="-Xms256m -Xmx1024m -XX:MaxPermSize=256m $CATALINA_OPTS $LOG_OPTS $SECURITY_OPTS $EXO_OPTS $IDE_OPTS $EXO_PROFILES $EXO_XML"
+CATALINA_OPTS="${CATALINA_OPTS} -Djava.net.preferIPv4Stack=true"
+# Disable EHCache update checker
+CATALINA_OPTS="${CATALINA_OPTS} -Dnet.sf.ehcache.skipUpdateCheck=true"
+CATALINA_OPTS="${CATALINA_OPTS} ${EXO_SERVER_XML_OPTS}"
 export CATALINA_OPTS
