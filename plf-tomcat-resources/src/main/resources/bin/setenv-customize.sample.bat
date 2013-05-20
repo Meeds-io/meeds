a21a36496071dbac8c280a9ba2964ef04342bf8b
Arnaud Héritier <aheritier@exoplatform.com>
2013-05-20 16:47:59 +0200

    PLF-5036 : Cleanup env var usage and especially PATHs related settings. Ensure that it is working with our without any space in the product path or in JAVA_HOME/JRE_HOME

--- plf-tomcat-resources/src/main/resources/bin/setenv-customize.sample.bat
+++ plf-tomcat-resources/src/main/resources/bin/setenv-customize.sample.bat
@@ -17,7 +17,7 @@
 @REM 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 @REM
 
-@echo off
+@ECHO off
 
 REM # ---------------------------------------------------------------------------
 REM
@@ -37,81 +37,81 @@ REM # JVM configuration
 REM # ---------------------------------------------------------------------------
 
 REM # Home directory of the JVM to use (Default : try to auto-compute it from existing java executable in path by default)
-REM SET "JAVA_HOME=C:\Program Files\Java\jdk6"
+REM SET JAVA_HOME=C:\Program Files\Java\jdk6
 
 REM # Maximum Heap Size to use (Default : 2g)
-REM SET "EXO_JVM_SIZE_MAX=1g"
+REM SET EXO_JVM_SIZE_MAX=1g
 
 REM # Minimum Heap Size to use (Default : 512m)
-REM SET "EXO_JVM_SIZE_MIN=512m""
+REM SET EXO_JVM_SIZE_MIN=512m
 
 REM # Size of the Permanent Generation. (Default : 256m)
-REM SET "EXO_JVM_PERMSIZE_MAX=128m"
+REM SET EXO_JVM_PERMSIZE_MAX=128m
 
 REM # Default locale language
-REM SET "EXO_JVM_USER_LANGUAGE=fr"
+REM SET EXO_JVM_USER_LANGUAGE=fr
 
 REM # Default locale region
-REM SET "EXO_JVM_USER_REGION=FR"
+REM SET EXO_JVM_USER_REGION=FR
 
 REM # Loads in-process debugging libraries to attach a debugger (also available with --debug option on start_eXo.bat script)
-REM SET "EXO_DEBUG=true"
+REM SET EXO_DEBUG=true
 
 REM # Listening port for the debugger
-REM SET "EXO_DEBUG_PORT=8000"
+REM SET EXO_DEBUG_PORT=8000
 
 REM # ---------------------------------------------------------------------------
 REM # PLATFORM configuration
 REM # ---------------------------------------------------------------------------
 
 REM # eXo Platform comes with different runtime profiles, enabling you to customize which modules you want to enable/disable
-REM SET "EXO_PROFILES=default,cluster,cluster-index-local"
+REM SET EXO_PROFILES=default,cluster,cluster-index-local
 
 REM # Assets versions used in static resources URLs. Useful to manage caches. (Default: The product version)
-REM SET "EXO_ASSETS_VERSION=42"
+REM SET EXO_ASSETS_VERSION=42
 
 REM # Main directory where are stored all data (Default: %CATALINA_BASE%\gatein\data) (also available with --data <path> option on start_eXo.bat script)
-REM SET "EXO_DATA_DIR=%HOMEPATH%\eXo-platform\data"
+REM SET EXO_DATA_DIR=%HOMEPATH%\eXo-platform\data
 
 REM # Activates the development mode of eXo platform (also available with --dev option on start_eXo.bat script)
-REM SET "EXO_DEV=true"
+REM SET EXO_DEV=true
 
 REM # Activates the JCR sessions leaks detector (Default: $EXO_DEV. true with --dev option on start_eXo.sh script)
-REM SET "EXO_JCR_SESSION_TRACKING=true"
+REM SET EXO_JCR_SESSION_TRACKING=true
 
 REM # -----------------------------------------------------------------------------
 REM # SMTP/Emails configuration
 REM # -----------------------------------------------------------------------------
 
 REM # Domain name used to produce absolute URLs in email notifications. (Default: http://localhost:8080)
-REM SET "EXO_DEPLOYMENT_URL=https://intranet.mycompany.com"
+REM SET EXO_DEPLOYMENT_URL=https://intranet.mycompany.com
 
 REM # Email display in "from" field of email notification. (Default: noreply@exoplatform.com)
-REM SET "EXO_EMAIL_FROM=intranet@mycompany.com"
+REM SET EXO_EMAIL_FROM=intranet@mycompany.com
 
 REM # SMTP Server hostname. (Default: localhost)
-REM SET "EXO_EMAIL_SMTP_HOST=smtp.gmail.com"
+REM SET EXO_EMAIL_SMTP_HOST=smtp.gmail.com
 
 REM # SMTP Server port. (Default: 25)
-REM SET "EXO_EMAIL_SMTP_PORT=465"
+REM SET EXO_EMAIL_SMTP_PORT=465
 
 REM # True to enable the secure (TLS) SMTP. See RFC 3207. (Default: false)
-REM SET "EXO_EMAIL_SMTP_STARTTLS_ENABLE=true"
+REM SET EXO_EMAIL_SMTP_STARTTLS_ENABLE=true
 
 REM # True to enable the SMTP authentication. (Default: false)
-REM SET "EXO_EMAIL_SMTP_AUTH=true"
+REM SET EXO_EMAIL_SMTP_AUTH=true
 
 REM # Username to send for authentication. (Default: <NONE>)
-REM SET "EXO_EMAIL_SMTP_USERNAME=account@gmail.com"
+REM SET EXO_EMAIL_SMTP_USERNAME=account@gmail.com
 
 REM # Password to send for authentication. (Default: <NONE>)
-REM SET "EXO_EMAIL_SMTP_PASSWORD=password"
+REM SET EXO_EMAIL_SMTP_PASSWORD=password
 
 REM # Specify the port to connect to when using the specified socket factory. (Default: <NONE>)
-REM SET "EXO_EMAIL_SMTP_SOCKET_FACTORY_PORT=465"
+REM SET EXO_EMAIL_SMTP_SOCKET_FACTORY_PORT=465
 
 REM # This class will be used to create SMTP sockets. (Default: <NONE>)
-REM SET "EXO_EMAIL_SMTP_SOCKET_FACTORY_CLASS=javax.net.ssl.SSLSocketFactory"
+REM SET EXO_EMAIL_SMTP_SOCKET_FACTORY_CLASS=javax.net.ssl.SSLSocketFactory
 
 REM # -----------------------------------------------------------------------------
 REM # JOD Converter configuration
@@ -121,59 +121,59 @@ REM
 REM # Requires to have openoffice/libreoffice server installed
 
 REM # Jod Converter activation (Default : true)
-REM SET "EXO_JODCONVERTER_ENABLE=false"
+REM SET EXO_JODCONVERTER_ENABLE=false
 
 REM # Comma separated list of ports numbers to use for open office servers used to convert documents.
 REM # One office server instance will be created for each port. (Default : 2002)
-REM SET "EXO_JODCONVERTER_PORTS=2002,2003,2004,2005"
+REM SET EXO_JODCONVERTER_PORTS=2002,2003,2004,2005
 
 REM # The absolute path to the office home on the server. (Default : Path automatically discovered based on the OS default locations)
 REM # Prefer to use the shortname syntax without spaces for the PATH
-REM SET "EXO_JODCONVERTER_OFFICEHOME=C:\Program Files\OpenOffice"
+REM SET EXO_JODCONVERTER_OFFICEHOME=C:\Program Files\OpenOffice
 
 REM # ---------------------------------------------------------------------------
 REM # Logs configuration
 REM # ---------------------------------------------------------------------------
 
 REM # Logback configuration file (http://logback.qos.ch/manual/configuration.html ) - For an advanced customization of logs
-REM SET "EXO_LOGS_LOGBACK_CONFIG_FILE=%CATALINA_BASE%/conf/logback.xml"
+REM SET EXO_LOGS_LOGBACK_CONFIG_FILE=%CATALINA_BASE%/conf/logback.xml
 
-REM SET "EXO_LOGS_DISPLAY_CONSOLE=true"
+REM SET EXO_LOGS_DISPLAY_CONSOLE=true
 
 REM # Enforce to display colorized logs in the console (Default: false for windows, true otherwise) (You can use --color and --nocolor options on start_eXo.bat to enforce the configuration)
-REM SET "EXO_LOGS_COLORIZED_CONSOLE=true"
+REM SET EXO_LOGS_COLORIZED_CONSOLE=true
 
 REM # ---------------------------------------------------------------------------
 REM # Tomcat configuration
 REM # ---------------------------------------------------------------------------
 
 REM # Explodes all wars in the webapps directory (Default: $EXO_DEV. true with --dev option on start_eXo.bat script)
-REM SET "EXO_TOMCAT_UNPACK_WARS=%EXO_DEV%"
+REM SET EXO_TOMCAT_UNPACK_WARS=%EXO_DEV%
 
 REM # -----------------------------------------------------------------------------
 REM # Advanced settings (We directly append some settings in CATALINA_OPTS)
 REM # -----------------------------------------------------------------------------
 
 REM # JVM HeapDumpOnOutOfMemoryError (Useful to debug but the dump is long to produce)
-REM SET "CATALINA_OPTS=%CATALINA_OPTS% -XX:+HeapDumpOnOutOfMemoryError"
-REM SET "CATALINA_OPTS=%CATALINA_OPTS% -XX:HeapDumpPath=%CATALINA_HOME%/logs/"
+REM SET CATALINA_OPTS=%CATALINA_OPTS% -XX:+HeapDumpOnOutOfMemoryError
+REM SET CATALINA_OPTS=%CATALINA_OPTS% -XX:HeapDumpPath="%CATALINA_HOME%/logs/"
 
 REM # JVM GC Details
-REM SET "CATALINA_OPTS=%CATALINA_OPTS% -XX:+PrintGCDetails"
-REM SET "CATALINA_OPTS=%CATALINA_OPTS% -Xloggc:%CATALINA_HOME%/logs/gc.log"
+REM SET CATALINA_OPTS=%CATALINA_OPTS% -XX:+PrintGCDetails
+REM SET CATALINA_OPTS=%CATALINA_OPTS% -Xloggc:"%CATALINA_HOME%/logs/gc.log"
 
 REM # JMX (Sample of configuration to activate it without SSL and with access/password credentials)
-REM SET "CATALINA_OPTS=%CATALINA_OPTS% -Dcom.sun.management.jmxremote=true"
-REM SET "CATALINA_OPTS=%CATALINA_OPTS% -Dcom.sun.management.jmxremote.ssl=false"
-REM SET "CATALINA_OPTS=%CATALINA_OPTS% -Dcom.sun.management.jmxremote.password.file=%CATALINA_HOME%/conf/jmxremote.password"
-REM SET "CATALINA_OPTS=%CATALINA_OPTS% -Dcom.sun.management.jmxremote.access.file=%CATALINA_HOME%/conf/jmxremote.access"
+REM SET CATALINA_OPTS=%CATALINA_OPTS% -Dcom.sun.management.jmxremote=true
+REM SET CATALINA_OPTS=%CATALINA_OPTS% -Dcom.sun.management.jmxremote.ssl=false
+REM SET CATALINA_OPTS=%CATALINA_OPTS% -Dcom.sun.management.jmxremote.password.file=%CATALINA_HOME%/conf/jmxremote.password
+REM SET CATALINA_OPTS=%CATALINA_OPTS% -Dcom.sun.management.jmxremote.access.file=%CATALINA_HOME%/conf/jmxremote.access
 REM # If you access to JMX though an SSH tunnel
-REM SET "CATALINA_OPTS=%CATALINA_OPTS% -Djava.rmi.server.hostname=localhost"
+REM SET CATALINA_OPTS=%CATALINA_OPTS% -Djava.rmi.server.hostname=localhost
 REM # If you are behind a proxy you may have to activate the JMX Remote Lifecycle Listener fixes the ports used by the JMX/RMI Server
 REM # For more details see the configuration file conf/server.xml
 
 REM # JCR Statistics
-REM SET "CATALINA_OPTS=%CATALINA_OPTS% -DJDBCWorkspaceDataContainer.statistics.enabled=true -DJCRStatisticsManager.persistence.timeout=30000"
+REM SET CATALINA_OPTS=%CATALINA_OPTS% -DJDBCWorkspaceDataContainer.statistics.enabled=true -DJCRStatisticsManager.persistence.timeout=30000
 
 REM # CRaSH Extension (change telnet and SSH ports)
 REM SET "CATALINA_OPTS=%CATALINA_OPTS% -Dcrash.telnet.port=12345"
--- plf-tomcat-resources/src/main/resources/bin/setenv.bat
+++ plf-tomcat-resources/src/main/resources/bin/setenv.bat
@@ -17,7 +17,7 @@
 @REM 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 @REM
 
-@echo off
+@ECHO off
 
 REM # ---------------------------------------------------------------------------
 REM #            /!\     DON'T MODIFY THIS FILE      /!\
@@ -36,189 +36,199 @@ REM # --------------------------------------------------------------------------
 REM #            /!\     DON'T MODIFY THIS FILE      /!\
 REM # ---------------------------------------------------------------------------
 
-rem Get standard environment variables
-if not exist "%CATALINA_BASE%\bin\setenv-customize.bat" goto setEnvCustomize
-call "%CATALINA_BASE%\bin\setenv-customize.bat"
-
-:setEnvCustomize
-rem Get standard Java environment variables
-if exist "%CATALINA_HOME%\bin\setclasspath.bat" goto okSetclasspath
-echo Cannot find "%CATALINA_HOME%\bin\setclasspath.bat"
-echo This file is needed to run this program
-goto end
-:okSetclasspath
-call "%CATALINA_HOME%\bin\setclasspath.bat" %1
-if errorlevel 1 goto end
-
 REM # We validate that Command extensions are available
 VERIFY other 2>nul
 SETLOCAL enableextensions
 IF ERRORLEVEL 1 (
   ECHO Unable to enable extensions
-  exit 1
+  PAUSE
+  EXIT 1
 )
 ENDLOCAL
+
+REM # Load custom settings
+IF NOT EXIST "%CATALINA_BASE%\bin\setenv-customize.bat" GOTO setEnvCustomize
+call "%CATALINA_BASE%\bin\setenv-customize.bat"
+:setEnvCustomize
+
+REM # Remove quotes from JAVA_HOME/JRE_HOME
+IF DEFINED JAVA_HOME SET JAVA_HOME=%JAVA_HOME:"=%
+IF DEFINED JRE_HOME SET JRE_HOME=%JRE_HOME:"=%
+
+REM # Find/Validate JAVA_HOME/JRE_HOME
+IF EXIST "%CATALINA_HOME%\bin\setclasspath.bat" GOTO okSetclasspath
+ECHO Cannot find %CATALINA_HOME%\bin\setclasspath.bat
+ECHO This file is needed to run this program
+PAUSE
+EXIT 1
+:okSetclasspath
+call "%CATALINA_HOME%\bin\setclasspath.bat" "%1"
+IF ERRORLEVEL 1 (
+  ECHO Unable to compute JAVA_HOME
+  PAUSE
+  EXIT 1
+)
 
 REM # ---------------------------------------------------------------------------
 REM # Default JVM configuration
 REM # ---------------------------------------------------------------------------
 
-IF NOT DEFINED EXO_JVM_SIZE_MAX SET "EXO_JVM_SIZE_MAX=2g"
-IF NOT DEFINED EXO_JVM_SIZE_MIN SET "EXO_JVM_SIZE_MIN=512m"
-IF NOT DEFINED EXO_JVM_PERMSIZE_MAX SET "EXO_JVM_PERMSIZE_MAX=256m"
-IF NOT DEFINED EXO_JVM_USER_LANGUAGE SET "EXO_JVM_USER_LANGUAGE=en"
-IF NOT DEFINED EXO_JVM_USER_REGION SET "EXO_JVM_USER_REGION=US"
-IF NOT DEFINED EXO_DEBUG SET "EXO_DEBUG=false"
-IF NOT DEFINED EXO_DEBUG_PORT SET "EXO_DEBUG_PORT=8000"
+IF NOT DEFINED EXO_JVM_SIZE_MAX SET EXO_JVM_SIZE_MAX=2g
+IF NOT DEFINED EXO_JVM_SIZE_MIN SET EXO_JVM_SIZE_MIN=512m
+IF NOT DEFINED EXO_JVM_PERMSIZE_MAX SET EXO_JVM_PERMSIZE_MAX=256m
+IF NOT DEFINED EXO_JVM_USER_LANGUAGE SET EXO_JVM_USER_LANGUAGE=en
+IF NOT DEFINED EXO_JVM_USER_REGION SET EXO_JVM_USER_REGION=US
+IF NOT DEFINED EXO_DEBUG SET EXO_DEBUG=false
+IF NOT DEFINED EXO_DEBUG_PORT SET EXO_DEBUG_PORT=8000
 
 REM # ---------------------------------------------------------------------------
 REM # Default EXO PLATFORM configuration
 REM # ---------------------------------------------------------------------------
 
-IF NOT DEFINED EXO_PROFILES SET "EXO_PROFILES=default"
-IF NOT DEFINED EXO_DEV SET "EXO_DEV=false"
-IF NOT DEFINED EXO_ASSETS_VERSION SET "EXO_ASSETS_VERSION=${project.version}"
-IF NOT DEFINED EXO_JCR_SESSION_TRACKING SET "EXO_JCR_SESSION_TRACKING=%EXO_DEV%"
-IF NOT DEFINED EXO_DATA_DIR SET "EXO_DATA_DIR=%CATALINA_BASE%\gatein\data"
+IF NOT DEFINED EXO_PROFILES SET EXO_PROFILES=default
+IF NOT DEFINED EXO_DEV SET EXO_DEV=false
+IF NOT DEFINED EXO_ASSETS_VERSION SET EXO_ASSETS_VERSION=${project.version}
+IF NOT DEFINED EXO_JCR_SESSION_TRACKING SET EXO_JCR_SESSION_TRACKING=%EXO_DEV%
+IF NOT DEFINED EXO_DATA_DIR SET EXO_DATA_DIR=%CATALINA_BASE%\gatein\data
 
 REM # ---------------------------------------------------------------------------
 REM # Default Logs configuration
 REM # ---------------------------------------------------------------------------
 
 REM # Default configuration for logs (using logback framework - http://logback.qos.ch/manual/configuration.html )
-IF NOT DEFINED EXO_LOGS_LOGBACK_CONFIG_FILE SET "EXO_LOGS_LOGBACK_CONFIG_FILE=%CATALINA_BASE%\conf\logback.xml"
-IF NOT DEFINED EXO_LOGS_DISPLAY_CONSOLE SET "EXO_LOGS_DISPLAY_CONSOLE=false"
-IF NOT DEFINED EXO_LOGS_COLORIZED_CONSOLE SET "EXO_LOGS_COLORIZED_CONSOLE="
+IF NOT DEFINED EXO_LOGS_LOGBACK_CONFIG_FILE SET EXO_LOGS_LOGBACK_CONFIG_FILE=%CATALINA_BASE%\conf\logback.xml
+IF NOT DEFINED EXO_LOGS_DISPLAY_CONSOLE SET EXO_LOGS_DISPLAY_CONSOLE=false
+IF NOT DEFINED EXO_LOGS_COLORIZED_CONSOLE SET EXO_LOGS_COLORIZED_CONSOLE=
 
 REM # ---------------------------------------------------------------------------
 REM # Default Tomcat configuration
 REM # ---------------------------------------------------------------------------
 
 REM # Global Tomcat settings
-IF NOT DEFINED EXO_TOMCAT_UNPACK_WARS SET "EXO_TOMCAT_UNPACK_WARS=%EXO_DEV%"
+IF NOT DEFINED EXO_TOMCAT_UNPACK_WARS SET EXO_TOMCAT_UNPACK_WARS=%EXO_DEV%
 
 REM # ---------------------------------------------------------------------------
 REM # Export the needed system properties for server.xml
 REM # ---------------------------------------------------------------------------
 
-SET "JAVA_OPTS=%JAVA_OPTS% -DEXO_TOMCAT_UNPACK_WARS=%EXO_TOMCAT_UNPACK_WARS% -DEXO_DEV=%EXO_DEV%"
+SET JAVA_OPTS=%JAVA_OPTS% -DEXO_TOMCAT_UNPACK_WARS=%EXO_TOMCAT_UNPACK_WARS% -DEXO_DEV=%EXO_DEV%
 
 REM # ---------------------------------------------------------------------------
 REM # Logs customization (Managed by slf4J\logback instead of tomcat-juli & co)
 REM # ---------------------------------------------------------------------------
 
 REM # Deactivate j.u.l
-SET "LOGGING_MANAGER=-Dnop"
+SET LOGGING_MANAGER=-Dnop
 REM # Add additional bootstrap entries for logging purpose using SLF4J+Logback
 REM # SLF4J deps
-SET "CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\lib\slf4j-api-${org.slf4j.version}.jar"
-SET "CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\lib\jul-to-slf4j-${org.slf4j.version}.jar"
+SET CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\lib\slf4j-api-${org.slf4j.version}.jar
+SET CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\lib\jul-to-slf4j-${org.slf4j.version}.jar
 REM # LogBack deps
-SET "CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\lib\logback-core-${ch.qas.logback.version}.jar"
-SET "CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\lib\logback-classic-${ch.qas.logback.version}.jar"
+SET CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\lib\logback-core-${ch.qas.logback.version}.jar
+SET CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\lib\logback-classic-${ch.qas.logback.version}.jar
 REM # Janino deps (used by logback for conditional processing in the config file)
-SET "CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\lib\janino-${org.codehaus.janino.version}.jar"
-SET "CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\lib\commons-compiler-${org.codehaus.janino.version}.jar"
+SET CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\lib\janino-${org.codehaus.janino.version}.jar
+SET CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\lib\commons-compiler-${org.codehaus.janino.version}.jar
 REM # Jansi deps for colorized output on windows
-SET "CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\lib\jansi-${org.fusesource.jansi.version}.jar"
+SET CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\lib\jansi-${org.fusesource.jansi.version}.jar
 
 REM # ---------------------------------------------------------------------------
 REM # Compute the CATALINA_OPTS
 REM # ---------------------------------------------------------------------------
 
 IF /I %EXO_DEBUG% EQU true (
-  SET "CATALINA_OPTS=%CATALINA_OPTS% -agentlib:jdwp=transport=dt_socket,address=%EXO_DEBUG_PORT%,server=y,suspend=n"
+  SET CATALINA_OPTS=%CATALINA_OPTS% -agentlib:jdwp=transport=dt_socket,address=%EXO_DEBUG_PORT%,server=y,suspend=n
 )
 
 IF /I %EXO_DEV% EQU true (
-  SET "CATALINA_OPTS=%CATALINA_OPTS% -Dorg.exoplatform.container.configuration.debug"
-  SET "CATALINA_OPTS=%CATALINA_OPTS% -Dexo.product.developing=true"
+  SET CATALINA_OPTS=%CATALINA_OPTS% -Dorg.exoplatform.container.configuration.debug
+  SET CATALINA_OPTS=%CATALINA_OPTS% -Dexo.product.developing=true
 )
 
 REM # JCR session leak detector
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Dexo.jcr.session.tracking.active=%EXO_JCR_SESSION_TRACKING%"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Dexo.jcr.session.tracking.active=%EXO_JCR_SESSION_TRACKING%
 
 REM # JVM memory allocation pool parameters
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Xms%EXO_JVM_SIZE_MIN% -Xmx%EXO_JVM_SIZE_MAX% -XX:MaxPermSize=%EXO_JVM_PERMSIZE_MAX%"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Xms%EXO_JVM_SIZE_MIN% -Xmx%EXO_JVM_SIZE_MAX% -XX:MaxPermSize=%EXO_JVM_PERMSIZE_MAX%
 
 REM # Reduce the RMI GCs to once per hour for Sun JVMs.
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000
 
 REM # Default user locale defined at JVM level
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Duser.language=%EXO_JVM_USER_LANGUAGE% -Duser.region=%EXO_JVM_USER_REGION%"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Duser.language=%EXO_JVM_USER_LANGUAGE% -Duser.region=%EXO_JVM_USER_REGION%
 
 REM # Network settings
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Djava.net.preferIPv4Stack=true"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Djava.net.preferIPv4Stack=true
 
 REM # Headless
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Djava.awt.headless=true"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Djava.awt.headless=true
 
 REM # Platform profiles
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Dexo.profiles=%EXO_PROFILES%"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Dexo.profiles=%EXO_PROFILES%
 
 REM # Platform paths
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Dexo.conf.dir.name=gatein\conf"
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Dexo.conf.dir=%CATALINA_BASE%\gatein\conf"
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Dgatein.conf.dir=%CATALINA_BASE%\gatein\conf"
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Dgatein.data.dir=%EXO_DATA_DIR%"
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Djava.security.auth.login.config=%CATALINA_BASE%\conf\jaas.conf"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Dexo.conf.dir.name=gatein\conf
+SET CATALINA_OPTS=%CATALINA_OPTS% -Dexo.conf.dir="%CATALINA_BASE%\gatein\conf"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Dgatein.conf.dir="%CATALINA_BASE%\gatein\conf"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Dgatein.data.dir="%EXO_DATA_DIR%"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Djava.security.auth.login.config="%CATALINA_BASE%\conf\jaas.conf"
 
 REM # JAVA_HOME is computed by setclasspath.bat if required
-if not exist "%JAVA_HOME%\bin\javac.exe" goto JavaHomeIsJRE
+IF NOT EXIST "%JAVA_HOME%\bin\javac.exe" GOTO JavaHomeIsJRE
 
 REM # We have a JDK
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Djre.lib=%JAVA_HOME%\jre\lib"
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Djavasrc=%JAVA_HOME%\src.zip"
-goto okLibAndSrcPaths
+SET CATALINA_OPTS=%CATALINA_OPTS% -Djre.lib="%JAVA_HOME%\jre\lib"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Djavasrc="%JAVA_HOME%\src.zip"
+GOTO okLibAndSrcPaths
 :JavaHomeIsJRE
 REM # We have a JRE
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Djre.lib=%JAVA_HOME%\lib"
-if not exist "%JAVA_HOME%\..\src.zip" goto okLibAndSrcPaths
+SET CATALINA_OPTS=%CATALINA_OPTS% -Djre.lib="%JAVA_HOME%\lib"
+IF NOT EXIST "%JAVA_HOME%\..\src.zip" GOTO okLibAndSrcPaths
 REM # This is a JRE inside a JDK
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Djavasrc=%JAVA_HOME%\..\src.zip"
+SET CATALINA_OPTS="%CATALINA_OPTS%" -Djavasrc="%JAVA_HOME%\..\src.zip"
 :okLibAndSrcPaths
 
 REM # Assets version
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Dgatein.assets.version=%EXO_ASSETS_VERSION%"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Dgatein.assets.version=%EXO_ASSETS_VERSION%
 
 REM # Logback configuration file
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Dlogback.configurationFile=%EXO_LOGS_LOGBACK_CONFIG_FILE%"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Dlogback.configurationFile="%EXO_LOGS_LOGBACK_CONFIG_FILE%"
 
 REM # Define the XML Parser
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Djavax.xml.stream.XMLOutputFactory=com.sun.xml.internal.stream.XMLOutputFactoryImpl -Djavax.xml.stream.XMLInputFactory=com.sun.xml.internal.stream.XMLInputFactoryImpl -Djavax.xml.stream.XMLEventFactory=com.sun.xml.internal.stream.events.XMLEventsFactoryImpl"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Djavax.xml.stream.XMLOutputFactory=com.sun.xml.internal.stream.XMLOutputFactoryImpl -Djavax.xml.stream.XMLInputFactory=com.sun.xml.internal.stream.XMLInputFactoryImpl -Djavax.xml.stream.XMLEventFactory=com.sun.xml.internal.stream.events.XMLEventsFactoryImpl
 
 REM # PLF-4968/JCR-2164 : Avoid Exception when starting with Java 7 (http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6804124)
-SET "CATALINA_OPTS=%CATALINA_OPTS% -Djava.util.Arrays.useLegacyMergeSort=true"
+SET CATALINA_OPTS=%CATALINA_OPTS% -Djava.util.Arrays.useLegacyMergeSort=true
 
 REM # Jod Converter activation
-IF DEFINED EXO_JODCONVERTER_ENABLE SET "CATALINA_OPTS=%CATALINA_OPTS% -Djodconverter.enable=%EXO_JODCONVERTER_ENABLE%"
+IF DEFINED EXO_JODCONVERTER_ENABLE SET CATALINA_OPTS=%CATALINA_OPTS% -Djodconverter.enable=%EXO_JODCONVERTER_ENABLE%
 REM # Comma separated list of ports numbers to use for open office servers used to convert documents.
-IF DEFINED EXO_JODCONVERTER_PORTS SET "CATALINA_OPTS=%CATALINA_OPTS% -Djodconverter.portnumbers=%EXO_JODCONVERTER_PORTS%"
+IF DEFINED EXO_JODCONVERTER_PORTS SET CATALINA_OPTS=%CATALINA_OPTS% -Djodconverter.portnumbers=%EXO_JODCONVERTER_PORTS%
 REM # The absolute path to the office home on the server.
-IF DEFINED EXO_JODCONVERTER_OFFICEHOME SET "CATALINA_OPTS=%CATALINA_OPTS% -Djodconverter.officehome=%EXO_JODCONVERTER_OFFICEHOME%"
+IF DEFINED EXO_JODCONVERTER_OFFICEHOME SET CATALINA_OPTS=%CATALINA_OPTS% -Djodconverter.officehome="%EXO_JODCONVERTER_OFFICEHOME%"
 
 REM # Domain name used to produce absolute URLs in email notifications.
-IF DEFINED EXO_DEPLOYMENT_URL SET "CATALINA_OPTS=%CATALINA_OPTS% -Ddomain.url=%EXO_DEPLOYMENT_URL%"
-REM # Email display in "from" field of email notification.
-IF DEFINED EXO_EMAIL_FROM SET "CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.from=%EXO_EMAIL_FROM%"
+IF DEFINED EXO_DEPLOYMENT_URL SET CATALINA_OPTS=%CATALINA_OPTS% -Ddomain.url=%EXO_DEPLOYMENT_URL%
+REM # Email display in from field of email notification.
+IF DEFINED EXO_EMAIL_FROM SET CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.from=%EXO_EMAIL_FROM%
 REM # SMTP Server hostname.
-IF DEFINED EXO_EMAIL_SMTP_HOST SET "CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.host=%EXO_EMAIL_SMTP_HOST%"
+IF DEFINED EXO_EMAIL_SMTP_HOST SET CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.host=%EXO_EMAIL_SMTP_HOST%
 REM # SMTP Server port.
-IF DEFINED EXO_EMAIL_SMTP_PORT SET "CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.port=%EXO_EMAIL_SMTP_PORT%"
+IF DEFINED EXO_EMAIL_SMTP_PORT SET CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.port=%EXO_EMAIL_SMTP_PORT%
 REM # True to enable the secure (TLS) SMTP. See RFC 3207.
-IF DEFINED EXO_EMAIL_SMTP_STARTTLS_ENABLE SET "CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.starttls.enable=%EXO_EMAIL_SMTP_STARTTLS_ENABLE%"
+IF DEFINED EXO_EMAIL_SMTP_STARTTLS_ENABLE SET CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.starttls.enable=%EXO_EMAIL_SMTP_STARTTLS_ENABLE%
 REM # True to enable the SMTP authentication.
-IF DEFINED EXO_EMAIL_SMTP_AUTH SET "CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.auth=%EXO_EMAIL_SMTP_AUTH%"
+IF DEFINED EXO_EMAIL_SMTP_AUTH SET CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.auth=%EXO_EMAIL_SMTP_AUTH%
 REM # Username to send for authentication.
-IF DEFINED EXO_EMAIL_SMTP_USERNAME SET "CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.username=%EXO_EMAIL_SMTP_USERNAME%"
+IF DEFINED EXO_EMAIL_SMTP_USERNAME SET CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.username="%EXO_EMAIL_SMTP_USERNAME%"
 REM # Password to send for authentication.
-IF DEFINED EXO_EMAIL_SMTP_PASSWORD SET "CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.password=%EXO_EMAIL_SMTP_PASSWORD%"
+IF DEFINED EXO_EMAIL_SMTP_PASSWORD SET CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.password="%EXO_EMAIL_SMTP_PASSWORD%"
 REM # Specify the port to connect to when using the specified socket factory.
-IF DEFINED EXO_EMAIL_SMTP_SOCKET_FACTORY_PORT SET "CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.socketFactory.port=%EXO_EMAIL_SMTP_SOCKET_FACTORY_PORT%"
+IF DEFINED EXO_EMAIL_SMTP_SOCKET_FACTORY_PORT SET CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.socketFactory.port=%EXO_EMAIL_SMTP_SOCKET_FACTORY_PORT%
 REM # This class will be used to create SMTP sockets.
-IF DEFINED EXO_EMAIL_SMTP_SOCKET_FACTORY_CLASS SET "CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.socketFactory.class=%EXO_EMAIL_SMTP_SOCKET_FACTORY_CLASS%"
+IF DEFINED EXO_EMAIL_SMTP_SOCKET_FACTORY_CLASS SET CATALINA_OPTS=%CATALINA_OPTS% -Dsmtp.socketFactory.class=%EXO_EMAIL_SMTP_SOCKET_FACTORY_CLASS%
 
 REM # Set the window name
-SET "TITLE=eXo Platform ${project.version}"
+SET TITLE=eXo Platform ${project.version}
 
 :end
